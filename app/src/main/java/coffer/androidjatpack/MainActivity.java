af1755c5c0c82ba6099dd24d6b818aeb8796bc13
coffer <zbq19930108@126.com>
2019-05-15 17:50:21 +0800

    code message : 完善反射Demo

--- app/src/main/java/coffer/androidjatpack/MainActivity.java
+++ app/src/main/java/coffer/androidjatpack/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import coffer.animdemo.AnimActivity;
+import coffer.reflectdemo.ReflectActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AnimActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 反射
+        findViewById(R.id.b2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ReflectActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/coffer/reflectdemo/Person.java
@@ -0,0 +1,34 @@
+package coffer.reflectdemo;
+
+import android.util.Log;
+
+/**
+ * @author：张宝全
+ * @date：2019-05-15
+ * @Description：
+ * @Reviser：
+ * @RevisionTime：
+ * @RevisionDescription：
+ */
+public class Person {
+
+    private static final String TAG = "reflect-tag";
+
+    private String name;
+
+    public Person(){
+        Log.e(TAG,"创建了一个无参Person实例");
+    }
+
+    public Person(String name){
+        Log.e(TAG,"创建了有参"+name);
+    }
+
+    private void setName1(){
+        Log.e(TAG,"调用了无参数方法");
+    }
+
+    protected void setName2(String name){
+        Log.e(TAG,"调用了有参："+name);
+    }
+}
