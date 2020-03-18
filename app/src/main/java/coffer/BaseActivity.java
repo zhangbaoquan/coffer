328812828605c47cb5465bfa2075b1fde3da4518
coffer <zbq19930108@126.com>
2020-03-18 10:19:51 +0800

    code message : test1

---
+++ app/src/main/java/coffer/BaseActivity.java
@@ -0,0 +1,22 @@
+package coffer;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author：张宝全
+ * @date：2020-03-18
+ * @Description：
+ * @Reviser：
+ * @RevisionTime：
+ * @RevisionDescription：
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
