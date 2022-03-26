21620fbb72aed7a388a0de2beea14545bafd5cf3
coffer <zbq19930108@126.com>
2022-03-26 18:01:56 +0800

    code message : 完善多渠道配置

--- app/src/main/java/com/kc/MainActivity.kt
+++ app/src/main/java/com/kc/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.kc
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.coffer.app.R
@@ -16,6 +18,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 输出渠道信息
+        val appInfo = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
+        val channelName = appInfo.metaData.getString("MTA_CHANNEL")
+        Log.d("channel_tag","channel = $channelName")
+
         findViewById<View>(R.id.btn).setOnClickListener { v ->
             Router.go(
                 v.context,
